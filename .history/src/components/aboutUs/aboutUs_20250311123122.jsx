44ae627829d86c03439553f58d7fdac46f1e9c22
Sah Menz <mensahemma670@gmail.com>
2025-03-11 12:37:51 +0100

    update

---
+++ .history/src/components/aboutUs/aboutUs_20250311123122.jsx
@@ -0,0 +1,83 @@
+import ourMission from "../../assets/images/ourMission.svg";
+import ourVision from "../../assets/images/ourVision.svg";
+import ourValues from "../../assets/images/ourValues.svg";
+import middleImageTop from "../../assets/images/middleImageTop.png";
+import middleImageLeft from "../../assets/images/middleImageLeft.png";
+import middleImageRight from "../../assets/images/middleImageRight.png";
+import aboutUsImageRight from "../../assets/images/aboutUsImageRight.png";
+import aboutUsImageLeft from "../../assets/images/aboutUsImageLeft.png";
+function AboutUs() {
+  return (
+    <div className="mt-5 container-adjustment">
+      <div className="text-center">
+        <div className="fs-43 fw-500 txt-a0">Our Guiding Principles</div>
+        <div className="fs-16 fw-400">
+          We modernize event planning while preserving African heritage,
+          ensuring seamless coordination, cultural authenticity, and memorable
+          celebrations.
+        </div>
+      </div>
+      <div className="mt-5 d-flex flex-column align-items-center justify-content-center gap-4 flex-lg-row justify-content-lg-between">
+        <div
+          className="bg-a0 w-25"
+          style={{ minWidth: "250px", borderRadius: "15px", padding: "25px" }}
+        >
+          <img
+            src={ourMission}
+            alt=""
+            style={{ borderRadius: "30px", backgroundColor: "#fff" }}
+          />
+          <div className="bg-a0 txt-ff fw-500 fs-23">Our Mission</div>
+          <div style={{ height: "100px", backgroundColor: "#A04D07" }}></div>
+          <div className="fs-16 fw-400 txt-ff bg-a0">
+            To empower event planners and individuals by providing a
+            comprehensive, culturally nuanced platform that makes every
+            celebration uniquely memorable, efficient, and stress-free.
+          </div>
+        </div>
+
+        <div
+          className="w-25"
+          style={{ minWidth: "250px", borderRadius: "15px", padding: "25px" }}
+        >
+          <img src={ourVision} alt="" />
+          <div className="txt-a0 fw-500 fs-23">Our Vision</div>
+          <div style={{ height: "100px" }}></div>
+          <div className="fs-16 fw-400 txt-00">
+            To be the leading platform for African-themed events, seamlessly
+            blending tradition with technology to create meaningful and
+            memorable celebrations.
+          </div>
+        </div>
+
+        <div
+          className="w-25"
+          style={{ minWidth: "250px", borderRadius: "15px", padding: "25px" }}
+        >
+          <img src={ourValues} alt="" />
+          <div className="txt-a0 fw-500 fs-23">Our Values</div>
+          <div style={{ height: "100px" }}></div>
+          <div className="fs-16 fw-400 txt-00">
+            We innovate to meet user needs, preserve African heritage, build a
+            strong community, and deliver excellence through a seamless
+            experience.
+          </div>
+        </div>
+      </div>
+      <div className="txt-73 fs-83 fw-500 mt-5 w-75">
+        Where <span className="txt-a0">Cultural Roots</span> Meet Modern Expression
+      </div>
+      <div className="d-flex align-items-center justify-content-center mt-5 flex-column">
+        <div><img src={aboutUsImageLeft}  style={{width:"100vw", maxWidth:"400px"}}/></div>
+        <div className="d-flex align-items-center justify-content-center gap-3 gap-md-5 mt-5 mt-md-0" style={{height:"400px"}}>
+          <div><img src={middleImageLeft} style={{width:"25vw", maxWidth:"400px"}}/></div>
+          <div className="align-self-start"><img src={middleImageTop} style={{width:"25vw", maxWidth:"400px"}}/></div>
+          <div><img src={middleImageRight} style={{width:"25vw", maxWidth:"400px"}}/></div>
+        </div>
+        <div><img src={aboutUsImageRight} style={{width:"100vw", maxWidth:"400px"}}/></div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ .history/src/components/howAwambeWorks/howAwambeWorks_20250308232856.jsx
@@ -0,0 +1,343 @@
+import { useState } from "react";
+import Collapse from "react-bootstrap/Collapse";
+import CustomSvg from "../customSvg/CustomSvg";
+import arrow from '../../assets/images/Arrow.svg'
+import guest from '../../assets/images/guest.svg'
+import register from '../../assets/images/register.svg'
+import planning from '../../assets/images/planning.svg'
+import gift from '../../assets/images/gift.svg'
+import invitation from '../../assets/images/invitation.svg'
+
+function HowAwambeWorks() {
+  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+  const [open4, setOpen4] = useState(false);
+
+  return (
+    <div className="container-adjustment">
+      <div>
+        <div className="d-flex align-items-center flex-column justify-content-center text-center">
+          <div className="txt-a0 fw-500 fs-43 mb-3">How Awambe Works</div>
+          <div className="fs-16 fw-400 col-6">
+            Our user-friendly platform guides you through every step of the
+            event planning process, ensuring that you remain in control from
+            start to finish.
+          </div>
+        </div>
+        <div>
+          <div className="d-flex justify-content-center" style={{height:"250px"}}>
+            <div className="d-flex col-4">
+              <div className="text-center d-flex flex-column align-items-center ">
+                <img src={register}/>
+                <div className="fs-19 fw-600 my-2">Register</div>
+                <div className=" fs-13 fw-400" style={{minWidth:"163px"}}>Quickly sign up, then access your personalized planning dashboard.</div>
+              </div>
+              <div>
+              <img src={arrow}/>
+              </div>
+            </div>
+            
+            
+            <div className="d-flex col-4 align-self-end">
+              <div className="text-center d-flex flex-column align-items-center ">
+                <img src={invitation}/>
+                <div className="fs-19 fw-600 my-2">Invitations</div>
+                <div className=" fs-13 fw-400" style={{minWidth:"163px"}}>Design and customize unique invitations with ease, then preview before sending</div>
+              </div>
+              <img src={arrow}/>
+            </div>
+
+
+          </div>
+          {/* <div></div> */}
+          {/* <div></div> */}
+        </div>
+      </div>  
+      <div> 
+        <div className="fs-43 fw-500 text-center mt-5 mb-4">
+          Frequently Asked Questions
+        </div>
+        <div className="d-flex flex-column align-items-center mb-5">
+          <div
+            onClick={() => setOpen(!open)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open}
+            className="bg-eb flex-column d-flex mt-3 col-10 align-items-center justify-content-between"
+            style={{
+              cursor: "pointer",
+              borderRadius: "20px",
+              height: open ? "fit-content" : "",
+            }}
+          >
+            {/* <div> */}
+            <div
+              className="d-flex align-items-center justify-content-between col-12"
+              style={{
+                padding: "20px 29px 20px 18px",
+              }}
+            >
+              <div
+                className="fw-400"
+                // style={{ width: "70vw" }}
+              >
+                What is Awambe?
+              </div>
+              <div>
+                {open ? (
+                  <div>
+                    <CustomSvg name="minus" />
+                  </div>
+                ) : (
+                  <div>
+                    <CustomSvg name="plus" />
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* </div> */}
+            <Collapse in={open}>
+              <div id="example-fade-text">
+                <div
+                  className="fw-400 txt-16 "
+                  style={{
+                    borderRadius: "20px",
+                    padding: "20px 29px 20px 18px",
+                  }}
+                >
+                  Awambe is an event planning platform designed to simplify
+                  African-themed celebrations. We offer customizable
+                  invitations, guest management tools, a curated vendor
+                  marketplace, and a seamless event coordination experience.
+                </div>
+              </div>
+            </Collapse>
+          </div>
+
+          <div
+            onClick={() => setOpen1(!open1)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open1}
+            className="bg-eb flex-column d-flex mt-3 col-10 align-items-center justify-content-between"
+            style={{
+              cursor: "pointer",
+              borderRadius: "20px",
+              height: open1 ? "fit-content" : "",
+            }}
+          >
+            {/* <div> */}
+            <div
+              className="d-flex align-items-center justify-content-between col-12"
+              style={{
+                padding: "20px 29px 20px 18px",
+              }}
+            >
+              <div
+                className="fw-400"
+                // style={{ width: "70vw" }}
+              >
+                Who can use Awambe?
+              </div>
+              <div>
+                {open1 ? (
+                  <div>
+                    <CustomSvg name="minus" />
+                  </div>
+                ) : (
+                  <div>
+                    <CustomSvg name="plus" />
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* </div> */}
+            <Collapse in={open1}>
+              <div id="example-fade-text">
+                <div
+                  className="fw-400 txt-16 "
+                  style={{
+                    borderRadius: "20px",
+                    padding: "20px 29px 20px 18px",
+                  }}
+                >
+                  Awambe is an event planning platform designed to simplify
+                  African-themed celebrations. We offer customizable
+                  invitations, guest management tools, a curated vendor
+                  marketplace, and a seamless event coordination experience.
+                </div>
+              </div>
+            </Collapse>
+          </div>
+
+          <div
+            onClick={() => setOpen2(!open2)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open2}
+            className="bg-eb flex-column d-flex mt-3 col-10 align-items-center justify-content-between"
+            style={{
+              cursor: "pointer",
+              borderRadius: "20px",
+              height: open2 ? "fit-content" : "",
+            }}
+          >
+            {/* <div> */}
+            <div
+              className="d-flex align-items-center justify-content-between col-12"
+              style={{
+                padding: "20px 29px 20px 18px",
+              }}
+            >
+              <div
+                className="fw-400"
+                // style={{ width: "70vw" }}
+              >
+                How does Awambe help with event planning?
+              </div>
+              <div>
+                {open2 ? (
+                  <div>
+                    <CustomSvg name="minus" />
+                  </div>
+                ) : (
+                  <div>
+                    <CustomSvg name="plus" />
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* </div> */}
+            <Collapse in={open2}>
+              <div id="example-fade-text">
+                <div
+                  className="fw-400 txt-16 "
+                  style={{
+                    borderRadius: "20px",
+                    padding: "20px 29px 20px 18px",
+                  }}
+                >
+                  Awambe is an event planning platform designed to simplify
+                  African-themed celebrations. We offer customizable
+                  invitations, guest management tools, a curated vendor
+                  marketplace, and a seamless event coordination experience.
+                </div>
+              </div>
+            </Collapse>
+          </div>
+
+          <div
+            onClick={() => setOpen3(!open3)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open3}
+            className="bg-eb flex-column d-flex mt-3 col-10 align-items-center justify-content-between"
+            style={{
+              cursor: "pointer",
+              borderRadius: "20px",
+              height: open3 ? "fit-content" : "",
+            }}
+          >
+            {/* <div> */}
+            <div
+              className="d-flex align-items-center justify-content-between col-12"
+              style={{
+                padding: "20px 29px 20px 18px",
+              }}
+            >
+              <div
+                className="fw-400"
+                // style={{ width: "70vw" }}
+              >
+                Can I create digital invitations on Awambe?
+              </div>
+              <div>
+                {open3 ? (
+                  <div>
+                    <CustomSvg name="minus" />
+                  </div>
+                ) : (
+                  <div>
+                    <CustomSvg name="plus" />
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* </div> */}
+            <Collapse in={open3}>
+              <div id="example-fade-text">
+                <div
+                  className="fw-400 txt-16 "
+                  style={{
+                    borderRadius: "20px",
+                    padding: "20px 29px 20px 18px",
+                  }}
+                >
+                  Awambe is an event planning platform designed to simplify
+                  African-themed celebrations. We offer customizable
+                  invitations, guest management tools, a curated vendor
+                  marketplace, and a seamless event coordination experience.
+                </div>
+              </div>
+            </Collapse>
+          </div>
+
+          <div
+            onClick={() => setOpen4(!open4)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open4}
+            className="bg-eb flex-column d-flex mt-3 col-10 align-items-center justify-content-between"
+            style={{
+              cursor: "pointer",
+              borderRadius: "20px",
+              height: open4 ? "fit-content" : "",
+            }}
+          >
+            {/* <div> */}
+            <div
+              className="d-flex align-items-center justify-content-between col-12"
+              style={{
+                padding: "20px 29px 20px 18px",
+              }}
+            >
+              <div
+                className="fw-400"
+                // style={{ width: "70vw" }}
+              >
+                How does guest management work on Awambe?
+              </div>
+              <div>
+                {open4 ? (
+                  <div>
+                    <CustomSvg name="minus" />
+                  </div>
+                ) : (
+                  <div>
+                    <CustomSvg name="plus" />
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* </div> */}
+            <Collapse in={open4}>
+              <div id="example-fade-text">
+                <div
+                  className="fw-400 txt-16 "
+                  style={{
+                    borderRadius: "20px",
+                    padding: "20px 29px 20px 18px",
+                  }}
+                >
+                  Awambe is an event planning platform designed to simplify
+                  African-themed celebrations. We offer customizable
+                  invitations, guest management tools, a curated vendor
+                  marketplace, and a seamless event coordination experience.
+                </div>
+              </div>
+            </Collapse>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HowAwambeWorks;
